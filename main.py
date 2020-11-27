2859fab4129031bdf4fea40942d35cd121f65809
NeilSCGH <neil.segard@gmail.com>
2020-11-27 11:55:39 +0100

    query made from json

--- main.py
+++ main.py
@@ -5,18 +5,31 @@ import requests, json, base64
 url = 'http://localhost:9200/shakespeare-catalog-2/_search'
 headers = {'Content-Type': 'application/json'}
 
-data = '{"query": {"match": {"text_entry.english": {"query": "The edge of war", "minimum_should_match": "2"}}}}'
-#data=data.encode('utf-8')
+query = {
+          "query":{
+              "match":{
+                "text_entry.english":{
+                    "query":"The edge of war",
+                    "minimum_should_match":"2"
+                }
+              }
+          }
+        }
+data=json.dumps(query)
 
 ##Making request
 print("Making request... ", end="")
-res = requests.get(url=url, data=data, headers=headers)
+try:
+  res = requests.get(url=url, data=data, headers=headers)
 
-if res.status_code == 200: #ok
-  content = res.content
-  content = json.loads(content.decode('utf-8'))
-  print("Done!\n")
-  print("Result:\n")
-  print(json.dumps(content, indent=2, sort_keys=True))
-else:
-  print("Failed! (Error {})".format(res.status_code))
+  if res.status_code == 200: #ok
+    content = res.content
+    content = json.loads(content.decode('utf-8'))
+    print("Done!\n")
+    print("Result:\n")
+    print(json.dumps(content, indent=2, sort_keys=True))
+  else:
+    print("Failed! (Error {})".format(res.status_code))
+except:
+    print("Failed! (Error making request)")
+
